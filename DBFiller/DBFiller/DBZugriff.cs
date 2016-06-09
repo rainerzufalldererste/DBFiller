de885efeebafde06561b8037efc1f70f90d56ff7
Kevin Lee <Klee@stud.hs-heilbronn.de>
2016-06-09 14:48:49 +0200

    added testmethod to write data into postgres

--- DBFiller/DBFiller/DBZugriff.cs
+++ DBFiller/DBFiller/DBZugriff.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-
+using Npgsql;
 
 namespace DBFiller
 {
     class DBZugriff
-    { 
-      
+    {
+        public void LoadData(NpgsqlConnection connection)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand();
+
+            cmd.Connection = connection;
+
+            foreach(Medikament med in Master.medikamente)
+            {
+                cmd.CommandText = "INSERT INTO Medikament (name, id) VALUES (" + med._name + ", " + med._id + ")";
+                cmd.ExecuteNonQuery();
+            }
+        }    
     }
 }
